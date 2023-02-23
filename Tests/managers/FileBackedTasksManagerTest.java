0a42e9b6549383e89c8e40dd49fb0212af8c2be7
Aleks <aleks312t@gmail.com>
2023-02-23 21:01:00 +0300

    feat: добавлена директория для тестов

---
+++ Tests/managers/FileBackedTasksManagerTest.java
@@ -0,0 +1,78 @@
+package managers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FileBackedTasksManagerTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void loadFromFile() {
+    }
+
+    @Test
+    void startFile() {
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void testAddTask() {
+    }
+
+    @Test
+    void testAddTask1() {
+    }
+
+    @Test
+    void returnTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void countEpics() {
+    }
+
+    @Test
+    void containEpic() {
+    }
+
+    @Test
+    void findSubTasks() {
+    }
+
+    @Test
+    void printNewTasks() {
+    }
+
+    @Test
+    void printInProgressTasks() {
+    }
+
+    @Test
+    void printDoneTasks() {
+    }
+
+    @Test
+    void deleteAllTasks() {
+    }
+
+    @Test
+    void printAllCodes() {
+    }
+}
